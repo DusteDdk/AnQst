e875576ca0af4690f616f0a43cfefa8b46b42cc0
DusteD <dusted@dusted.dk>
2026-05-22 12:02:54 +0200

    Version 1.7.5

--- AnQst/AnQstWidget/AnQstWebBase/src/AnQstWebBaseAbi.h
+++ AnQst/AnQstWidget/AnQstWebBase/src/AnQstWebBaseAbi.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define ANQST_WEBBASE_ABI_STAMP "_1.7.4"
-#define ANQST_WEBBASE_NAMESPACE anqstwebbase_1_7_4
+#define ANQST_WEBBASE_ABI_STAMP "_1.7.5"
+#define ANQST_WEBBASE_NAMESPACE anqstwebbase_1_7_5
